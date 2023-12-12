2395015b0779032234428e81ecf8305601f33b4b
陈厚萍 <chenhouping@boco.com>
2023-12-12 15:34:11 +0800

    工程初始化

---
+++ docs/.vuepress/nav/navbar.js
@@ -0,0 +1,25 @@
+module.exports = [
+    {
+        text: '主页',
+        link: '/main/'
+    },
+    {
+        text: '了解更多',
+        ariaLabel: '了解更多',
+        items: [
+            {
+                text: 'AAA',
+                items: [
+                    {
+                        text: 'BBB',
+                        link: '/main/BBB.html'
+                    },
+                    {
+                        text: 'CCC',
+                        link: '/main/CCC.html'
+                    }
+                ]
+            }
+        ]
+    }
+];
---
+++ docs/index.md
@@ -0,0 +1,10 @@
+---
+home: true
+heroImage: /logo.png
+actionText: 进入主页 →
+actionLink: /main/
+features:
+- title: 标题
+  details: 标题的描述
+---
+
